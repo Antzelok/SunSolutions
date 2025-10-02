793a3bc7c132d14f080bce542bb076a72e85e45d
Aggelosk <akermezo@yahoo.com>
2025-10-02 13:45:19 +0300

    Add SolarCard component to Homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,37 @@
-const HomePage = () => {
-    return ( <>Homepage</> );
-}
- 
+import { Metadata } from "next";
+import Image from "next/image";
+import SolarCard from "@/components/product/solar-card";
+import { getAllProducts } from "@/lib/actions/product.actions";
+import { Product } from "@/types";
+
+export const metadata: Metadata = {
+  title: "Αρχική",
+};
+
+const HomePage = async () => {
+  const products: Product[] = await getAllProducts();
+
+  return (
+    <>
+      <main className="flex flex-col items-center">
+        {/* Hero image */}
+        <Image
+          src="/images/sun.jpeg"
+          alt="Sun"
+          width={1920}
+          height={1080}
+          priority
+          className="w-full object-cover"
+        />
+      </main>
+      {/* Product card */}
+      <div className="flex flex-wrap justify-center my-15 gap-15">
+        {products.map((product) => (
+          <SolarCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default HomePage;
